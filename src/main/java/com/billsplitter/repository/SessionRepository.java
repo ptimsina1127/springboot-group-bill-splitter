a913f8be75525fc84a277f0163c65d3d7c151bb6
Pravat K Timsina <pravatktimsina@gmail.com>
2026-05-24 10:37:29 -0700

    Fix missing Optional import in SessionRepository

--- src/main/java/com/billsplitter/repository/SessionRepository.java
+++ src/main/java/com/billsplitter/repository/SessionRepository.java
@@ -3,6 +3,7 @@ package com.billsplitter.repository;
 import com.billsplitter.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, String> {
